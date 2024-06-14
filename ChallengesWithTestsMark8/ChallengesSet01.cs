0442eef44c138ba50262ce473f183a840bf7603b
Derrick <Avirajoesph@yahoo.com>
2024-06-14 14:42:40 -0500

    fixedproject

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -1,49 +1,67 @@
 ﻿using System;
 
-namespace ChallengesWithTestsMark8
-{
-    public class ChallengesSet01
+
+
+    
+    
+      namespace ChallengesWithTestsMark8
     {
-        public bool AreTwoNumbersTheSame(int num1, int num2)
+        public class ChallengesSet01
         {
-            if(num1 == num2)
+            public bool AreTwoNumbersTheSame(int num1, int num2)
             {
-                return true;
+                if (num1 == num2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
-            else
+
+            public double Subtract(double minuend, double subtrahend)
             {
-                return false;
+                double difference = minuend - subtrahend;
+                return difference;
+            }
+
+            public int Add(int number1, int number2)
+            {
+                int sum = number1 + number2;
+                return sum;
+            }
+
+            public int GetSmallestNumber(int number1, int number2)
+            {
+                return Math.Min(number1, number2);
+            }
+
+            public long Multiply(long factor1, long factor2)
+            {
+                long quotient = factor1 * factor2;
+                return quotient;
+            }
+
+            public string GetGreeting(string nameOfPerson)
+            {
+                String greeting = $"Hello, {nameOfPerson}!";
+
+                if (string.IsNullOrEmpty(nameOfPerson))
+                {
+                    return "Hello!";
+                }
+                else
+                {
+                    return greeting;
+                }
+            }
+
+            public string GetHey()
+            {
+                return "HEY!";
             }
         }
 
-        public double Subtract(double minuend, double subtrahend)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Add(int number1, int number2)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int GetSmallestNumber(int number1, int number2)
-        {
-            throw new NotImplementedException();
-        }
-
-        public long Multiply(long factor1, long factor2)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetGreeting(string nameOfPerson)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetHey()
-        {
-            throw new NotImplementedException();
-        }
     }
-}
+
